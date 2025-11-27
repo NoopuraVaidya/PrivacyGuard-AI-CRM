a2158ace8e6b117e79e63fe8d1d513fd05963422
Noopura Vaidya <noopuravaidya14@gmail.com>
2025-11-27 14:38:22 -0500

    Change default port from 5000 to 5001

--- app.py
+++ app.py
@@ -303,5 +303,5 @@ def create_app() -> Flask:
 
 if __name__ == "__main__":
     app = create_app()
-    port = int(os.getenv("PORT", "5000"))
+    port = int(os.getenv("PORT", "5001"))
     app.run(host="0.0.0.0", port=port, debug=True)
